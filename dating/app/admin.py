990b08fd9856c12e5a1af6d0d39e15f62e2a2064
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2025-08-08 13:28:42 +0545

    Add: adding on admin.py

--- dating/app/admin.py
+++ dating/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import userDetails, Contact, FAQ, Home
+from .models import userDetails, Contact, FAQ, Home, Message
 # Register your models here.
 admin.site.register(Home)
 class UserDetailsAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class UserDetailsAdmin(admin.ModelAdmin):
 admin.site.register(userDetails, UserDetailsAdmin),
 admin.site.register(Contact)
 admin.site.register(FAQ)
+admin.site.register(Message)
