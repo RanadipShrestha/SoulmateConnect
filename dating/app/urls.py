bbd61289c238d501bed9bbebd560db8a63631b55
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2025-08-08 13:30:30 +0545

    Fix: fixing url issue

--- dating/app/urls.py
+++ dating/app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', index, name="index"),
+    path("", index, name="index"),
     path("register/", registerPage, name="register"),
     path("login/", loginPage, name="login"),
     path("aboutus/", aboutUs, name="aboutus"),
     path("match/",  matching_page, name="match"),
     path("message/", message, name="message"),
-    path("conatctus/", contactUs, name="conatctus")
+    path("conatctus/", contactUs, name="conatctus"),
+    path('<int:receiver_id>/', chat_room, name='chat_room'),
 ]
